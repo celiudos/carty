6c703ebbcb27a6373bdab3045dc0ad6123f6b54b
Felipe de Melo <felipe.demelo@interacso.com>
2022-07-13 18:00:55 +0200

    feat(envio-e-pacotes): retirar na loja form

--- pages/entrega-retirar-na-loja.tsx
+++ pages/entrega-retirar-na-loja.tsx
@@ -1,13 +1,34 @@
-import FormEnvioRetirarNaLoja from "@components/forms/FormEnvioRetirarNaLoja";
+import IconLink from "@components/iconLink/IconLink";
 import Layout from "@components/layout/Layout";
-import { Grid } from "@mui/material";
+import {
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  Switch,
+  Typography,
+} from "@mui/material";
 
 export default function Index() {
   return (
     <Layout>
       <Grid container spacing={2}>
+        <Grid item lg={3}>
+          <IconLink
+            iconName="akar-icons:location"
+            text="Endereço da Loja"
+            href="entregas"
+          />
+        </Grid>
         <Grid item lg>
-          <FormEnvioRetirarNaLoja />
+          <Typography gutterBottom variant="h2">
+           Deseja permitir a retirada dos produtos na sua loja física?
+          </Typography>
+          <Typography gutterBottom variant="h6">
+            Alguns clientes preferem buscar o produto para economizar na taxa de entrega.
+          </Typography>
+          <FormGroup>
+            <FormControlLabel control={<Switch />} label="Ativado" />
+          </FormGroup>
         </Grid>
       </Grid>
     </Layout>
